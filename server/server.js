718f2e4a49de4dac82fcd4d4fb57eb5f88afd6af
James Guo <james@MacBook-Pro-4.local>
2018-04-17 23:54:31 +0800

    add test /todos route

--- server/server.js
+++ server/server.js
@@ -1,23 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const mongoose = require('./db/mongoose').mongoose;
 const Todos = require('./model/todos').Todos;
 const Users = require('./model/users').Users;
 
-var newTodo = new Todos({
-  text: 'first thing to do'
+var app = new express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var newTodo = new Todos({
+    text: req.body.text
+  });
+  newTodo.save().then((doc)=>{
+    console.log(JSON.stringify(doc,undefined,2));
+    res.send(doc);
+  },(err)=>{
+    console.log('Unable to save to db',err);
+    res.status(400).send(err);
+  });
 });
 
-newTodo.save().then((doc)=>{
-  console.log(JSON.stringify(doc,undefined,2));
-},(err)=>{
-  console.log('Unable to save to db',err);
-});
+app.listen(3000,()=>{
+  console.log('Starting server');
+})
 
-var newUser = new Users({
-  email:'nflsgyx@163.com',
-});
+// var newUser = new Users({
+//   email:'nflsgyx@163.com',
+// });
+//
+// newUser.save().then((doc)=>{
+//   console.log(JSON.stringify(doc,undefined,2));
+// },(err)=>{
+//   console.log('Unable to save to db',err);
+// });
 
-newUser.save().then((doc)=>{
-  console.log(JSON.stringify(doc,undefined,2));
-},(err)=>{
-  console.log('Unable to save to db',err);
-});
+module.exports = {app};
