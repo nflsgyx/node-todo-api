6c462f6de0d716ff6dfad8360ed06028af2ac4ca
James Guo <james@MacBook-Pro-4.local>
2018-04-18 10:11:09 +0800

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const mongoose = require('./db/mongoose').mongoose;
 const Todos = require('./model/todos').Todos;
@@ -29,6 +30,22 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(err);
   });
 })
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  };
+  Todos.findById(id).then((todo)=>{
+    if (!todo) {
+      res.status(404).send();
+    } else {
+      res.send({todo});
+    }
+  },(err)=>{
+    res.status(400).send();
+  });
+});
 
 app.listen(3000,()=>{
   console.log('Starting server');
